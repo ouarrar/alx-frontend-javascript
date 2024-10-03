7ed2ccea6f8f88fa266a0b19042a8a05924c3e34
Abdelouahed Ouarrar <ouarrar.abdelouahed@gmail.com>
2024-10-03 22:46:09 +0100

    0x02-ES6_classes/

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,32 @@
+export default class Pricing {
+  /* eslint-disable no-underscore-dangle */
+
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
